52f4470b8058cc0659eb2ece6c9371a36757fbe2
98Ananya <109644257+98Ananya@users.noreply.github.com>
2022-07-20 20:41:52 +0530

    Adding HelloCode.cs file

---
+++ HelloCode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSharpSolutions
+{
+    class HiCode
+    {
+         static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World");
+
+        }
+    }
+}
